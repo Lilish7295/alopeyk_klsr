67f13a4abd341ec8e17d60ab2eb42309c3050e84
Lilish7295 <leylishams7831@gmail.com>
2024-02-27 13:58:03 +0330

    TripCostView added

--- delivery/urls.py
+++ delivery/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import PackageView
+from .views import PackageView, TripCostView
 
 
 urlpatterns = [
 
     path('package/view/', PackageView.as_view(),
         name='pakage_view'),
+
+    path('trip/cost/', TripCostView.as_view(),
+        name='trip_cost'),
 
 ]
--- delivery/views.py
+++ delivery/views.py
@@ -1,3 +1,5 @@
+import requests
+from django.utils import timezone
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics, status, permissions, serializers
@@ -19,3 +21,107 @@ class PackageView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class TripCostView(APIView):
+
+    MAP_BOX_DIRECTION_API_URL = 'https://platform.neshan.org/panel/api-key'
+    def process_mapbox_respons(self, respons):
+        
+        api_key = 'web.40aa9a585436415aab34d0287217c47c'
+        base_url = 'https://platform.neshan.org/'
+        cost_per_kilometer = 3000
+        total_cost = respons.json()['routes'][0]['distance']/1000*cost_per_kilometer
+        params = {
+            'origin' : '{},{}'.format((respons.json()['waypoints'][0]['location'][1]),
+                (respons.json()['waypoints'][0]['location'][0])),
+            'destination' : '{},{}'.format((respons.json()['waypoints'][1]['location'][1]),
+                (respons.json()['waypoints'][1]['location'][0])),
+            'key' : api_key
+        }
+        distance_response = requests.get(base_url, params=params)
+        distance_data = distance_response.json()
+        return distance_data
+    
+    def get_weather_condition(self, latitud, longitud):
+
+        api_key = '58d1fb0dbc6c6790007a4fa59ec1d973'
+        base_url = 'https://home.openweathermap.org/'
+        params = {
+            'lat' : latitud,
+            'lon' : longitud,
+            'appid' : api_key
+        }
+        weather_response = requests.get(base_url, params=params)
+        weather_data = weather_response.json()
+        weather_condition = 'bad' if any(keyword in 
+            weather_data['weather'][0]['description'].lower() 
+            for keyword in ['rain', 'snow']) else 'good'
+        return weather_condition
+    
+    def is_holiday(self, pick_date):
+
+        api_key = 'https://api.keybit.ir/time'
+        base_url = 'https://api.keybit.ir/time/?timezone=asia/tehran'
+        params = {
+            'year' : pick_date.year,
+            'month' : pick_date.month,
+            'day' : pick_date.day,
+            'key' : api_key
+        }
+        holiday_response = requests.get(base_url, params=params)
+        holiday_data = holiday_response.json()
+        is_holiday = holiday_data['holiday']
+        return is_holiday
+    
+    def is_in_tehran(latitud, longitud):
+
+        tehran_longitud_range = (51.22,51.73)
+        tehran_latitud_range = (35.42,35.90)
+        is_in_longitud_range = tehran_longitud_range[0] <= longitud <= tehran_longitud_range[1]
+        is_in_latitud_range = tehran_latitud_range[0] <= latitud <= tehran_latitud_range[1]
+        return is_in_longitud_range and is_in_latitud_range
+    
+    def post(self, requst):
+
+        try:
+            origin_lat = requst.data.get('origin_lat')
+            origin_long = requst.data.get('origin_long')
+            destination_lat = requst.data.get('destination_lat')
+            destination_long = requst.data.get('destination_long')
+            origin = '{},{}'.format(origin_lat, origin_long)
+            destination = '{},{}'.format(destination_lat,destination_long)
+            mapbox_api_key = 'web.40aa9a585436415aab34d0287217c47c'
+            url = self.MAP_BOX_DIRECTION_API_URL.format(origin=origin,
+                destination=destination, api_key=mapbox_api_key)
+            response = requests.get(url)
+            distance_data = self.process_mapbox_respons(response)
+            origin_weather_condition = self.get_weather_condition(origin)
+            destination_weather_condition = self.get_weather_condition(destination)
+            total_cost = self.process_mapbox_respons(response)
+            pick_date = timezone.now()
+            
+            if self.is_holiday(pick_date) or origin_weather_condition == 'bad' or destination_weather_condition == 'bad':
+                total_cost += 5000
+
+            new_package = Package(
+                origin = origin,
+                destination = destination,
+                pick_date = timezone.now(),
+                customer = requst.user.customer,
+                courier = None,
+                condition = 'wating'
+            )
+            new_package.save()
+
+            if not (self.is_in_tehran(float(origin_long),
+                float(origin_lat) and self.is_in_tehran(float(destination_long),
+                float(destination_lat)))):
+                return Response({'error':'مبدا یا مقصد خارج از محدوده‌ی تهران است'},
+                    status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response({'total_cost':total_cost, 'distance_data':distance_data,
+                    'package_id':new_package.id}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'error':str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
